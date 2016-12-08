1f0f56a31997a45778fff2f13541a3e24cb4bb13
demoriz <demoriz@gmail.com>
2016-12-08 15:27:12 +0300

    add cookie, error correction

--- lib/others/cookie.php
+++ lib/others/cookie.php
@@ -3,26 +3,24 @@
 namespace Swebs\Helper\Others;
 
 use Bitrix\Main\Context;
+use Bitrix\Main\Web;
 
 class Cookie
 {
     public static function getCookie($strName = false)
     {
         $obRequest = Context::getCurrent()->getRequest();
-        $obOut = '';
         if ($strName === false) {
-            $obOut = $obRequest->getCookieList();
-        } else {
-            $obOut = $obRequest->getCookie($strName);
+            return $obRequest->getCookieList();
         }
 
-        return $obOut;
+        return $obRequest->getCookie($strName);
     }
 
     public static function setCookie($strName, $strValue, $strDomain = '')
     {
         $obContext = Context::getCurrent();
-        $obCookie = new Cookie($strName, $strValue);
+        $obCookie = new Web\Cookie($strName, $strValue);
         if (!empty($strDomain)) {
             $obCookie->setDomain($strDomain);
         }
