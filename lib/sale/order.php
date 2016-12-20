112ff5f43ac41a89219555d9a1e8a30a7efa8c06
demoriz <demoriz@gmail.com>
2016-12-20 18:30:03 +0300

    add methods for deliveries and pay systems

--- lib/sale/order.php
+++ lib/sale/order.php
@@ -2,7 +2,9 @@
 
 namespace Swebs\Helper\Sale;
 
+use Bitrix\Main\Context;
 use Bitrix\Main\Loader;
+use Bitrix\Sale;
 
 Loader::includeModule('sale');
 
@@ -33,5 +35,52 @@ class Order
         }
 
         return true;
+    }
+
+    public static function getDeliveries($intUserID = NULL)
+    {
+        global $USER;
+
+        if ($intUserID == NULL) {
+            $intUserID = $USER->GetID();
+        }
+
+        if ($intUserID == NULL) {
+            $intUserID = \CSaleUser::GetAnonymousUserID();
+        }
+
+        $strSiteId = Context::getCurrent()->getSite();
+        $obOrder = Sale\Order::create($strSiteId, $intUserID);
+
+        $obShipment = $obOrder->getShipmentCollection()->createItem();
+        $arDelivery = Sale\Delivery\Services\Manager::getRestrictedObjectsList($obShipment);
+
+        return $arDelivery;
+    }
+
+    public static function getPaySystems($intUserID = NULL, $intDeliveryID = false)
+    {
+        global $USER;
+
+        if ($intUserID == NULL) {
+            $intUserID = $USER->GetID();
+        }
+
+        if ($intUserID == NULL) {
+            $intUserID = \CSaleUser::GetAnonymousUserID();
+        }
+
+        $strSiteId = Context::getCurrent()->getSite();
+        $obOrder = Sale\Order::create($strSiteId, $intUserID);
+
+        if (is_numeric($intDeliveryID)) {
+            $obShipment = $obOrder->getShipmentCollection()->createItem();
+            $obShipment->setField('DELIVERY_ID', $intDeliveryID);
+        }
+
+        $obPayment = $obOrder->getPaymentCollection()->createItem();
+        $arPayment = Sale\PaySystem\Manager::getListWithRestrictions($obPayment);
+
+        return $arPayment;
     }
 }
